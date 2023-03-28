7e1ac0de57b2e94ed29bd0b01a443fc38173f6f9
ShivamSingh226 <shivamsingh226@gmail.com>
2023-03-28 13:38:57 +0530

    Added new files

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -1,14 +1,21 @@
 import { useSelector } from "react-redux";
 import { selectAllPosts } from "./postsSlice";
+import PostAuthor from "./PostAuthor";
+import TimeAgo from "./TimeAgo";
 import React from 'react'
 
 const PostsList = () => {
     const posts=useSelector(selectAllPosts);
-
-    const renderedposts=posts.map(post=>(
+    const orderedposts=posts.slice().sort((a,b)=>b.date.localeCompare(a.date));
+    
+    const renderedposts=orderedposts.map(post=>(
         <article key={post.id}>
             <h3>{post.title}</h3>
             <p>{post.content.substring(0,100)}</p>
+            <p className="postCredit">
+                <PostAuthor userId={post.userId}/>
+                <TimeAgo timestamp={post.date}/>
+            </p>
         </article>
     ))
   return (
--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,8 +1,24 @@
 import { createSlice, nanoid } from "@reduxjs/toolkit";
+import { sub } from "date-fns";
 
 const initialState=[
-    {id:'1',title:'Its about Redux',content:"I am learning at a great pace."},
-    {id:'2',title:'Its about Redux as well',content:"Helikopter Helikopter!! Parakofer Paarakofer."}
+    {id:'1',title:'Its about Redux',content:"I am learning at a great pace.",date:sub(new Date(),{minutes:10}).toISOString(),
+     reactions:{
+        thumbsUp:0,
+        wow:0,
+        heart:0,
+        rocket:0,
+        coffee:0
+     }},
+    {id:'2',title:'Its about Redux as well',content:"Helikopter Helikopter!! Parakofer Paarakofer.",date:sub(new Date(),{minutes:5}).toISOString(),
+    reactions:{
+        thumbsUp:0,
+        wow:0,
+        heart:0,
+        rocket:0,
+        coffee:0
+    }
+}
 ]
 
 const postsSlice=createSlice({
@@ -19,14 +35,22 @@ const postsSlice=createSlice({
                     id:nanoid(),
                     title,
                     content,
+                    date:new Date().toISOString(),
                     userId
                 }
             }
+        }
+       },
+       reactionAdded(state,action){
+        const {postId, reaction}=action.payload;
+        const existingpost=state.find(post=>post.id===postId);
+        if(existingpost){
+            existingpost.reactions[reaction]++;
         }
        }
     }
 })
 export const selectAllPosts=(state)=>state.posts;
 
-export const {postsAdded}=postsSlice.actions;
+export const {postsAdded,reactionAdded}=postsSlice.actions;
 export default postsSlice.reducer;
