549a5ebcc5b3b72bc0975d93d4065ef020cbe4eb
ShivamSingh226 <shivamsingh226@gmail.com>
2023-03-28 16:28:02 +0530

    Fix the bug

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -3,7 +3,7 @@ import { selectAllPosts } from "./postsSlice";
 import PostAuthor from "./PostAuthor";
 import TimeAgo from "./TimeAgo";
 import React from 'react'
-
+import ReactionButton from "./ReactionButton";
 const PostsList = () => {
     const posts=useSelector(selectAllPosts);
     const orderedposts=posts.slice().sort((a,b)=>b.date.localeCompare(a.date));
@@ -16,6 +16,7 @@ const PostsList = () => {
                 <PostAuthor userId={post.userId}/>
                 <TimeAgo timestamp={post.date}/>
             </p>
+            <ReactionButton post={post}/>
         </article>
     ))
   return (
