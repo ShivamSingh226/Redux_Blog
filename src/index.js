33bdeef1e467495e3c92337ea482921c1b7825e6
ShivamSingh226 <shivamsingh226@gmail.com>
2023-04-21 14:22:27 +0530

    Finally made the project

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import {store} from './app/store'
-
+import { fetchUsers } from './features/Users/UsersSlice';
+store.dispatch(fetchUsers());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
